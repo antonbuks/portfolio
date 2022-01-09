ed1b80baa5b2e98e7c5534ecc4d5eba581d26a9e
Anton Buksa <anton.buksa@infomaniak.com>
2022-01-09 18:26:10 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,120 @@
-import * as React from "react"
+import * as THREE from 'three'
+import { Detailed, Environment, useGLTF } from "@react-three/drei"
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
+import React, { Suspense, useRef, useState } from "react"
+import "../style.css"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+function Banana({ index, z, speed }) {
+  const ref = useRef()
+  // useThree gives you access to the R3F state model
+  const { viewport, camera } = useThree()
+  // getCurrentViewport is a helper that calculates the size of the viewport
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, -z])
+  const { nodes, materials } = useGLTF("/banana-v1-transformed.glb")
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+  // Local component state, it is safe to mutate because it's fixed data
+  const [data] = useState({
+    // Randomly distributing the objects along the vertical
+    y: THREE.MathUtils.randFloatSpread(height * 2),
+    // This gives us a random value between -1 and 1, we will multiply it with the viewport width
+    x: THREE.MathUtils.randFloatSpread(2),
+    // How fast objects spin, randFlost gives us a value between min and max, in this case 8 and 12
+    spin: THREE.MathUtils.randFloat(8, 12),
+    // Some random rotations, Math.PI represents 360 degrees in radian
+    rX: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
+  })
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+  // useFrame executes 60 times per second
+  useFrame((state, dt) => {
+    // Make the X position responsive, slowly scroll objects up at the Y, distribute it along the Z
+    // dt is the delta, the time between this frame and the previous, we can use it to be independent of the screens refresh rate
+    // We cap dt at 0.1 because now it can't accumulate while the user changes the tab, it will simply stop
+    if (dt < 0.1)
+      ref.current.position.set(
+        index === 0 ? 0 : data.x * width,
+        (data.y += dt * speed),
+        -z
+      )
+    // Rotate the object around
+    ref.current.rotation.set(
+      (data.rX += dt / data.spin),
+      Math.sin(index * 1000 + state.clock.elapsedTime / 10) * Math.PI,
+      (data.rZ += dt / data.spin)
+    )
+    // If they're too far up, set them back to the bottom
+    if (data.y > height * (index === 0 ? 4 : 1))
+      data.y = -(height * (index === 0 ? 4 : 1))
+  })
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+    <Detailed ref={ref} distances={[0, 65, 80]}>
+      <mesh
+        geometry={nodes.banana_high.geometry}
+        material={materials.skin}
+        material-emissive="#ff9f00"
       />
-    </main>
+      <mesh
+        geometry={nodes.banana_mid.geometry}
+        material={materials.skin}
+        material-emissive="#ff9f00"
+      />
+      <mesh
+        geometry={nodes.banana_low.geometry}
+        material={materials.skin}
+        material-emissive="#ff9f00"
+      />
+    </Detailed>
   )
 }
 
-export default IndexPage
+function Bananas({
+  speed = 1,
+  count = 80,
+  depth = 80,
+  easing = (x) => Math.sqrt(1 - Math.pow(x - 1, 2)),
+}) {
+  return (
+    // No need for alpha and antialias (faster), dpr clamps the resolution to 1.5 (also faster than full resolution)
+    <Canvas
+      gl={{ alpha: false, antialias: false }}
+      dpr={[1, 1.5]}
+      camera={{ position: [0, 0, 10], fov: 20, near: 0.01, far: depth + 15 }}
+    >
+      <color attach="background" args={["#ffbf40"]} />
+      <spotLight
+        position={[10, 20, 10]}
+        penumbra={1}
+        intensity={3}
+        color="orange"
+      />
+      {/* Using cubic easing here to spread out objects a little more interestingly, i wanted a sole big object up front ... */}
+      {Array.from(
+        { length: count },
+        (_, i) => <Banana key={i} index={i} z={Math.round(easing(i / count) * depth)} speed={speed} /> /* prettier-ignore */
+      )}
+      <Environment preset="sunset" />
+      {/* Multisampling (MSAA) is WebGL2 antialeasing, we don't need it (faster) */}
+      <EffectComposer multisampling={0}>
+        <DepthOfField
+          target={[0, 0, 60]}
+          focalLength={0.5}
+          bokehScale={11}
+          height={700}
+        />
+      </EffectComposer>
+    </Canvas>
+  )
+}
+
+export default () => {
+  const [speed, set] = useState(1)
+  return (
+    <main>
+      <Suspense fallback={null}>
+        <Bananas speed={speed} />
+      </Suspense>
+    </main>
+  )
+}
